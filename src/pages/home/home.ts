6ecd2f4b497d5b00bee490971c319435887eae5a
Wagner Silva <wagner.s02@outlook.com>
2018-10-08 15:56:42 -0300

    08/10/2018-16:00:00h

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,19 +2,47 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 import { MapaDetalhesPage } from '../mapa-detalhes/mapa-detalhes'
-
+import { Geolocation } from '@ionic-native/geolocation';
+declare var google;
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  constructor(public navCtrl: NavController) { }
-  ionViewDidLoad() { }
+  startPosition:any;
+  directionsDisplay:any;
+  constructor(public navCtrl: NavController,private geolocation: Geolocation) { }
+  ionViewDidLoad() { 
+    this.getMapByGeolocation();
+  }
 
   abrirDetalhes() {
     //console.log(page);
     //this.navCtrl.push(FilmesDetalhesPage, { id: filme.id });
     this.navCtrl.push(MapaDetalhesPage);
-
   }
+  getMapByGeolocation() {
+
+    this.geolocation.getCurrentPosition()
+      .then((resp) => {
+        this.startPosition = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
+
+        const mapOptions = {
+          zoom: 18,
+          center: this.startPosition,
+          disableDefaultUI: true,
+          mapTypeId: 'roadmap'
+        }
+
+        var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+        this.directionsDisplay.setMap(map);
+
+        const marker = new google.maps.Marker({
+          position: this.startPosition,
+          map: map
+        });
+      }).catch((error) => {
+        console.log('Erro ao recuperar sua posição', error);
+      });
+    }
 }
